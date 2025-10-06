16fafe55c48d7e8ea12f57b46344dd79be886a09
gokulakrishna <gokulakrishna578@gmail.com>
2025-10-06 20:23:50 +0530

    Initial frontend push: register/login/reset flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 import Success from './pages/Success';
@@ -8,7 +10,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ForgotPassword />} />
+        <Route path="/" element={<Register />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
         <Route path="/success" element={<Success />} />
